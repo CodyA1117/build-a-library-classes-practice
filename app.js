7623283be0efc9c580412514bd7d6bd13b947d93
CodyA1117 <adamscody079@gmail.com>
2025-05-11 13:15:40 -0500

    added setter for checkedout, and added two methods for the library.

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 class Media {
-    constructor(media){
-      this._book = book;
-      this._cd = cd;
-      this._movie = movie;
+    constructor(title, isCheckedOut = false, ratings = []){
+      this._title = title;
+      this._isCheckedOut = isCheckedOut;
+      this._ratings = ratings;
   
     }
+    get title(){
+      return this._title;
+    }
+  
+    get isCheckedOut(){
+      return this._isCheckedOut;
+    }
+  
+    get ratings(){
+      return this._ratings;
+    }
+
+    set isCheckedOut(value){
+      this._isCheckedOut = value;
+
+    }
+
+    toggleCheckOutStatus(){
+      this._isCheckedOut = !this._isCheckedOut;
+    }
+
+    getAverageRating(){
+      
+    }
+  
   
   }
